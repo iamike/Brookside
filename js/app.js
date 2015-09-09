7fd09a362dc7a59a043bb3c2d8a0aeccfc048edb
iamike <dai.mike@gmail.com>
2015-09-09 11:02:17 +0800

    upate

--- js/app.js
+++ js/app.js
@@ -19,6 +19,28 @@ var app = {
             left: -(width - domwidth) / 2,
             top: -(height - domheight) / 2
         });
+
+
+
+        window.blockMenuHeaderScroll = false;
+$(window).on('touchstart', function(e)
+{
+    if ($(e.target).closest('#mobileMenuHeader').length == 1)
+    {
+        blockMenuHeaderScroll = true;
+    }
+});
+$(window).on('touchend', function()
+{
+    blockMenuHeaderScroll = false;
+});
+$(window).on('touchmove', function(e)
+{
+    if (blockMenuHeaderScroll)
+    {
+        e.preventDefault();
+    }
+});
     },
     showMode:function(){
         var $modeSelector = $("#modeSelector");
