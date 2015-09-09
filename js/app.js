8d4b3660dd95d7c644cfb1c048a788cf66020a6e
iamike <dai.mike@gmail.com>
2015-09-09 11:03:40 +0800

    update

--- js/app.js
+++ js/app.js
@@ -22,25 +22,7 @@ var app = {
 
 
 
-        window.blockMenuHeaderScroll = false;
-$(window).on('touchstart', function(e)
-{
-    if ($(e.target).closest('#mobileMenuHeader').length == 1)
-    {
-        blockMenuHeaderScroll = true;
-    }
-});
-$(window).on('touchend', function()
-{
-    blockMenuHeaderScroll = false;
-});
-$(window).on('touchmove', function(e)
-{
-    if (blockMenuHeaderScroll)
-    {
-        e.preventDefault();
-    }
-});
+        
     },
     showMode:function(){
         var $modeSelector = $("#modeSelector");
