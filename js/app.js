fc4cb6f2162d137bddde60e91fd76a0e81b59e50
iamike <dai.mike@gmail.com>
2015-09-10 17:09:00 +0800

    update

--- js/app.js
+++ js/app.js
@@ -24,40 +24,124 @@ var app = {
 
 
     },
-    showMode:function(){
-        var $modeSelector = $("#modeSelector");
-            $modeSelector.fadeIn(2000);
-    }
+    //single
+    showSpot: function() {
+        $('#spot').fadeIn();
+    },
+    showSwipeHint: function() {
+        $('#swipeHint').fadeIn();
+    },
+    showResult: function() {
 
+    },
+    showMode: function() {
+        var $modeSelector = $("#modeSelector");
+        $modeSelector.fadeIn(2000);
+    },
+    //double 
+    doubleModeStep1: function() {
+        $('.step1').show();
+    },
+    doubleModeStep2: function() {
+        $('.step1').hide();
+    },
+    doubleModeStep3: function() {
+        $('.step2').show();
+    },
+    doubleModeStep4: function() {
+        $('.step2').hide();
+    },
+    detectShake: function() {
+        //create a new instance of shake.js.
+        var myShakeEvent = new Shake({
+            threshold: 15
+        });
+
+        // start listening to device motion
+        myShakeEvent.start();
+
+        // register a shake event
+        window.addEventListener('shake', shakeEventDidOccur, false);
+
+        //shake event callback
+        function shakeEventDidOccur() {
+
+            //put your own code here etc.
+            exportRoot.gotoAndPlay();
+        }
+    }
 };
 
 
 
 //$(function() {
 
-    //creatjs touch event
-    createjs.Touch.enable(stage, true, false);
+//creatjs touch event
+createjs.Touch.enable(stage, true, false);
 
-    //init the canvas animation when it ready.
-    init();
+//init the canvas animation when it ready.
+init();
 
-    //make screen viewport center
-    app.resize();
+//make screen viewport center
+app.resize();
 
-    //add swpie event
-    $("#start").swipe({
-        //Generic swipe handler for all directions
-        swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+//add swpie event
+$("#start").swipe({
+    //Generic swipe handler for all directions
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
 
-                exportRoot.gotoAndPlay("toSence");
+        exportRoot.gotoAndPlay("toSence");
 
-                $("#start").hide();
-
-
-        },
-        //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold: 150
-    });
+        $("#start").hide();
 
 
+    },
+    //Default is 75px, set to 0 for demo so any distance triggers swipe
+    threshold: 150
+});
+
+//Single Mode
+//add swpie event
+$("#spot").swipe({
+    //Generic swipe handler for all directions
+    tap: function(event, target) {
+        exportRoot.gotoAndPlay();
+        $("#spot").hide();
+    }
+});
+$('#swipeHint').swipe({
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+        exportRoot.gotoAndPlay();
+        $('#swipeHint').hide();
+    },
+    threshold: 200
+})
+$('.option').swipe({
+    tap: function(event, target) {
+        console.log(target.id);
+        //exportRoot.gotoAndPlay("showResult1");
+    }
+})
+//double Mode
+//add swpie event
+$("#QRcode").swipe({
+    //Generic swipe handler for all directions
+    tap: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+        exportRoot.gotoAndPlay();
+
+        // $("#start").hide();
+
+    }
+});
+$("#spot").swipe({
+    //Generic swipe handler for all directions
+    tap: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+        exportRoot.gotoAndPlay();
+
+        // $("#start").hide();
+
+    }
+});
 //});
